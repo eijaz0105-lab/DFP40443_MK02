6a9288aa5e70a74b98d7f3854ccc3eb976ad1b62
eijaz0105-lab <eijaz0105@gmail.com>
2026-01-20 15:13:03 +0800

    Add files via upload

---
+++ DIT4E 18DIT24F1050 EIJAZ IZZUDIN BIN SHAIPUL SHAHANI/biodata.php
@@ -0,0 +1,16 @@
+<?php
+$nama = "EIJAZ IZZUDIN BIN SHAIPUL SHAHANI";
+$nomatrik = "18DIT24F1050";
+$kelas = "DIT4E";
+$stage ="DIPLOMA POLITEKNIK TUANKU SYED SIRAJUDDIN";
+$sem = "SEMESTER 4";
+?>
+
+<img src="eijaz.jpg" width="150">
+
+<p>Nama: <?= $nama ?></p>
+<p>No Matrik: <?= $nomatrik ?></p>
+<p>Kelas: <?= $kelas ?></p>
+<p>Peringkat: <?= $stage ?></p>
+<P>Sem: <?= $sem ?></p>
+<p>Program Diploma Teknologi Maklumat</p>
---
+++ DIT4E 18DIT24F1050 EIJAZ IZZUDIN BIN SHAIPUL SHAHANI/index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Profile Saya</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+<body>
+    <div class="container">
+<h1>Profile Saya</h1>
+<?php include 'biodata.php'; ?>
+</div>
+
+</body>
+</html>
